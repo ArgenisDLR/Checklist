b23a061189fd017e84c91cd23396f139f22b8711
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-05-22 16:15:24 -0400

    setup label text to indexpath and uiview to tag

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -23,6 +23,14 @@ class ChecklistViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     
+    if let label = cell.viewWithTag(1000) as? UILabel {
+      if indexPath.row == 0 {
+        label.text = "Run a marathon"
+      } else {
+        label.text = "Sleep"
+      }
+    }
+    
     return cell
   }
 
